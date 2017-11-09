215e27fe256b722e650aa739c02637b00ec248e8
Fernando Robledo <fernando.robledo@opinno.com>
2017-11-09 20:43:56 +0100

    Initial commit

---
+++ src/InvalidArgumentException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Fernando Robledo <fernando.robledo@opinno.com>.
+ */
+
+namespace Overdesign\PsrCache;
+
+
+class InvalidArgumentException extends CacheException implements \Psr\Cache\InvalidArgumentException
+{
+
+}
